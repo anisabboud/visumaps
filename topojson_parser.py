2c923950d20c63c7a02e134c8ac1e8987aba4652
Anis Abboud <anis.abboud@gmail.com>
2014-06-06 00:50:37 -0400

    save geocoded Geonames ID of each entity in the world map in data/world-topo-min-attrs.tsv by running topojson_parser.py

--- topojson_parser.py
+++ topojson_parser.py
@@ -6,7 +6,8 @@ Created on May 22, 2014
 Parses a topojson file into a TSV table: ID    properties.
 '''
 
-FILE = 'data/world-topo-min.json'
+INPUT_FILE = 'data/world-topo-min.json'
+OUTPUT_FILE = 'data/world-topo-min-attrs.tsv'
 
 import json
 import urllib
@@ -14,7 +15,7 @@ import urllib2
 
 ids = []
 queries = []
-data = json.load(open(FILE))
+data = json.load(open(INPUT_FILE))
 for country in data['objects']['countries']['geometries']:
     country_id = country['id']
     name = country['properties']['name']
@@ -26,12 +27,18 @@ url = 'http://localhost:9999/geocode?q=%s' % all_queries
 response = urllib2.urlopen(url)
 data = json.loads(response.read())
 
-print '\t'.join(['id', 'name', 'geonameId'])
+output_lines = ['\t'.join(['id', 'name', 'geonameId'])]
 errors = []
 for country_id, name in zip(ids, queries):
     if name not in data:
         errors.append('Geocoder did not return a response for: %s' % name)
         continue
     geonameId = data[name]
-    print '\t'.join([str(country_id), name, str(geonameId)])
+    output_lines.append('\t'.join([str(country_id), name, str(geonameId)]))
+
+print 'Printing output to %s.' % OUTPUT_FILE
+with open(OUTPUT_FILE, 'w') as output_file:
+    output_file.writelines([line.encode('utf-8') + '\n' for line in output_lines])
+print 'Errors:'
 print '\n'.join(errors)
+
